8df9fc95fed31ee6f815a2268ba6497ae9d4c065
Abhijeet Tripathi <127136051+Abhijeet-Tripathi@users.noreply.github.com>
2023-10-18 17:28:55 +0530

    Update Dropdown.js

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import styles from "./Dropdown.module.css";
-
+//Below Component is receiving the option as prop.
 export default function Dropdown({ options }) {
-  const [display, setDisplay] = useState(false);
-  const [value, setValue] = useState("Select");
+  const [display, setDisplay] = useState(false);//This State helps in conditional rendering of the Options Array
+  const [value, setValue] = useState("Select");//This state stores Value of the Dropdown box,Initially set to Select.
 
+  //Below function on Clicking the Options Set the value of the dropbox to that  of the option selected.
   function handleSelection(e) {
     e.preventDefault();
     setValue(e.target.value);
@@ -23,6 +24,7 @@ export default function Dropdown({ options }) {
         >
           <option value={value}>{value}</option>
         </select>
+        {/* Conditional Rendering->Which becomes true when mouseOver Event is Active over the dropbox */}
         {display &&
           options.map((item,index) => (
             <option key={index}
@@ -38,6 +40,7 @@ export default function Dropdown({ options }) {
           ))}
 
       </div>
+      {/* Here we are simply showing the Value Selected */}
         <h2>Selected Option: <u>{value}</u></h2>
       
     </div>
