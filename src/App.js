1f7b2cb3fce8c8c08041c3e2595670054cc71f6e
Abhijeet Tripathi <127136051+Abhijeet-Tripathi@users.noreply.github.com>
2023-10-18 17:29:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import Dropdown from './Dropdown';
 import './App.css';
 
 function App() {
+  //Below is an Array containing the Options of Dropdown
   const choiceArray=["Yes","Probably Not","Never","Maybe","Always"];
   return (
     <>
     <h1>Should you use a dropdown?</h1>
+    {/* The Options Array is now being passed as prop to the Dropdown Component */}
       <Dropdown options={choiceArray}/>
     </>
   );
